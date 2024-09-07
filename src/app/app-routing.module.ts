a5e31fddd3393b6a38452ff440a32890475d30a6
Biswajeet <biswajeet163@gmail.com>
2024-09-07 22:50:11 +0530

    bad appssss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { CComponent } from './c/c.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'a', component: AComponent }, // Default route
+  { path: 'b', component: BComponent },
+  { path: 'c', component: CComponent },
+  { path: '**', redirectTo: 'a' } // Wildcard route for a 404 page (redirects to home page)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
